87d735ae04b5e25cf1f12552e57c8f8df2414f81
NearMaick <maick_a_s@msn.com>
2021-06-06 13:56:35 -0300

    style(web): responsive: Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Maick Souza</Text>
-        <Text color="gray.300" fontSize="small">
-          maick_a_s@msn.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Maick Souza</Text>
+          <Text color="gray.300" fontSize="small">
+            maick_a_s@msn.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Maick Souza" src="https://github.com/NearMaick.png" />
     </Flex>
