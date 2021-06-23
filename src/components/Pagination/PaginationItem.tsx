2bfa1a8bf242013e7fa16df1281af1d498b4bf5b
NearMaick <maick_a_s@msn.com>
2021-06-23 16:03:14 -0300

    feat(web): change pagination

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -3,10 +3,12 @@ import { Button } from "@chakra-ui/button";
 interface PaginationItemProps {
   number: number;
   isCurrent?: boolean;
+  onPageChange: (page: number) => void
 }
 
 export function PaginationItem ({ 
-  isCurrent = false, 
+  isCurrent = false,
+  onPageChange, 
   number 
 }: PaginationItemProps) {
   if(isCurrent) {
@@ -35,7 +37,8 @@ export function PaginationItem ({
       bg="gray.700"
       _hover={{
         bg: 'gray.500'
-      }}       
+      }}  
+      onClick={() => onPageChange(number)}     
     >
       {number}
     </Button>
